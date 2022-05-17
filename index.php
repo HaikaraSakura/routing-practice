c3f264f00d7a0aab9970a377b241cbc522ca7b1b
森太桜 <87011031+TaroMori0321@users.noreply.github.com>
2022-05-17 18:25:18 +0900

    Update index.php

--- index.php
+++ index.php
@@ -7,22 +7,21 @@ error_reporting(E_ALL);
 
 include './vendor/autoload.php';
 
-use \League\Route\Router;
-use \Psr\Http\Message\ResponseInterface;
-use \Psr\Http\Message\ServerRequestInterface;
-
 // 任意のディレクトリ名に変更すること
 const BASE_ROUTE = '/routing_practice';
 
 // Requestオブジェクトを生成
 $request = \Laminas\Diactoros\ServerRequestFactory::fromGlobals();
 
-$router = new Router();
+$router = new \League\Route\Router();
 
 // ルーティングを登録
 $router->map('GET', BASE_ROUTE . '/', new \App\IndexAction($response))->setName('IndexAction');
 
 $router->map('GET', BASE_ROUTE . '/products[/]', new \App\IndexAction($response))->setName('ProductsAction');
+
+// URL末尾にスラッシュがあったら、スラッシュのないURLにリダイレクト
+$router->middleware($container->get(\App\RemoveLastSlash::class));
 
 // Responseオブジェクトを生成
 $response = $router->dispatch($request);
